d29362f0a6ac5d1c890c81d4c533c818d28ff622
yating5250 <yating5250@gmail.com>
2025-08-13 23:16:36 +0800

    0813-y4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import homebanner from '../images/img-Home/HomeBanner.jpg'
+import '../sass/scss/home.scss'
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <section className="homebanner">
+        <figure>
+          <img src={homebanner} alt="home banner" />
+        </figure>
+
+        <div className="homeslogan"> 
+          <h2>在世界的浪潮中， 自由前行</h2>
+          <form name="home-b-form" id="home-b-form" method="post" accept-charset="UTF-8">
+            <input type="button" value="Start" className="home-b-button" />
+          </form>
+        </div>
+
+        <div className="homesideword">
+          <p>floating your own way</p>
+        </div>
+      </section>
+    </main>
   )
 }
 
