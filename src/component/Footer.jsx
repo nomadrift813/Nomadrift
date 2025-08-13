6820ced450cdd72e20933ee7e628939f4e98ca87
Jun-ccc Name <xuanjun0110@gmail.com>
2025-08-13 23:13:49 +0800

    0813-J-footer-jsx-scss

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import '../sass/scss/footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+    <nav>
+      <ul class="Sitemap">
+        <li>
+          <a href="#">關於我們</a>
+          <ul>
+            <li><a href="#">認識漂遊牧</a></li>
+            <li><a href="#">加入我們</a></li>
+            <li><a href="#">聯絡我們</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">會員中心</a>
+          <ul>
+            <li><a href="#">加入會員</a></li>
+            <li><a href="#">登入/註冊</a></li>
+            <li><a href="#">我的帳號</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">最新消息</a>
+          <ul>
+            <li><a href="#">E-mail</a></li>
+            <li><a href="#">Subscribe</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <h4>亞洲第一數位遊牧平台 <span>｜© 2025 Nomadrift. All rights reserved.</span></h4>
+    </footer>
   )
 }
 
