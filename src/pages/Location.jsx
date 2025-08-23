1e014621fc12686c815174e23910b99f883eb44a
Jun-ccc <xuanjun0110@gmail.com>
2025-08-24 00:31:02 +0800

    0823-J

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -2,7 +2,23 @@ import '../sass/scss/location.scss'
 
 const Location = () => {
   return (
-    <div>Location</div>
+    <main>
+      <section id="locBanner">
+        <div className='locSlogan'>
+          <p className='l-t-1'>Start your next journey here</p>
+          <p className='l-t-2'>熱門地點</p>
+          <div className="locScroll">
+            <img src="/img-Location/scroll.svg" alt="" />
+          </div>
+        </div>
+
+
+
+        <p className='locBanner-side-word'>where is the next</p>
+
+      </section>
+    </main>
+
   )
 }
 
