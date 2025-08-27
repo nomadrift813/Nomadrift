a15b715f7ea72b202fb8af1f77a036fa48e929c2
nomadrift813 <nomadrift813@gmail.com>
2025-08-27 10:57:12 +0800

    0827-J-home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,22 +30,22 @@ const Home = () => {
 
             <button className="home-b-form" type="button">
               <span>Start</span>
-              <img src="/img-Home/home-s-right.svg" alt="" />
+              <img src="./img-Home/home-s-right.svg" alt="" />
             </button>
           </div>
     
 
         <p className='banner-side-word'>floating your own way</p>
         <div className="homescroll">
-          <img src="/img-Home/homescroll.svg" alt="" />
+          <img src="./img-Home/homescroll.svg" alt="" />
         </div>
 
         <div className="banner-line">
-          <img src="/img-Home/banner-line.svg" alt="" />
+          <img src="./img-Home/banner-line.svg" alt="" />
         </div>
 
         <div className="homeboat-b">
-          <img src="/img-Home/boat-b.svg" alt="" />
+          <img src="./img-Home/boat-b.svg" alt="" />
         </div>
       </section>
 
@@ -58,13 +58,13 @@ const Home = () => {
             數位遊牧平台</p>
         </header>
         <div className='h-a-boxs'>
-          <article><figure><img src="/img-Home/advico-1.svg" alt="" /></figure><h3>全中介面</h3><p>亞洲第一數位遊牧<br />
+          <article><figure><img src="./img-Home/advico-1.svg" alt="" /></figure><h3>全中介面</h3><p>亞洲第一數位遊牧<br />
             揪團社群網頁</p></article>
-          <article><figure><img src="/img-Home/advico-2.svg" alt="" /></figure><h3>資訊整合</h3><p>整合揪團&資源<br />
+          <article><figure><img src="./img-Home/advico-2.svg" alt="" /></figure><h3>資訊整合</h3><p>整合揪團&資源<br />
             一站式查詢</p></article>
-          <article><figure><img src="/img-Home/advico-3.svg" alt="" /></figure><h3>跨國交流</h3><p>與世界各地游牧者<br />
+          <article><figure><img src="./img-Home/advico-3.svg" alt="" /></figure><h3>跨國交流</h3><p>與世界各地游牧者<br />
             互動學習</p></article>
-          <article><figure><img src="/img-Home/advico-4.svg" alt="" /></figure><h3>快速揪團</h3><p>一鍵開團<br />
+          <article><figure><img src="./img-Home/advico-4.svg" alt="" /></figure><h3>快速揪團</h3><p>一鍵開團<br />
             立刻找到同行夥伴</p></article>
         </div>
 
@@ -89,13 +89,13 @@ const Home = () => {
 
         <div className='home-lo-boxs'>
           <figure className="home-city-grid">
-            <img src="/img-Home/location-1.jpg" alt="" />
-            <img src="/img-Home/location-2.jpg" alt="" />
-            <img src="/img-Home/location-3.jpg" alt="" />
-            <img src="/img-Home/location-4.jpg" alt="" />
-            <img src="/img-Home/location-5.jpg" alt="" />
-            <img src="/img-Home/location-6.jpg" alt="" />
-            <img src="/img-Home/location-7.jpg" alt="" />
+            <img src="./img-Home/location-1.jpg" alt="" />
+            <img src="./img-Home/location-2.jpg" alt="" />
+            <img src="./img-Home/location-3.jpg" alt="" />
+            <img src="./img-Home/location-4.jpg" alt="" />
+            <img src="./img-Home/location-5.jpg" alt="" />
+            <img src="./img-Home/location-6.jpg" alt="" />
+            <img src="./img-Home/location-7.jpg" alt="" />
           </figure>
 
           <button className='home-all-spot'>
@@ -103,7 +103,7 @@ const Home = () => {
           </button>
 
           <div className='h-location-line'>
-            <img src="/img-Home/location-line.svg" alt="" />
+            <img src="./img-Home/location-line.svg" alt="" />
           </div>
 
         </div>
@@ -122,16 +122,16 @@ const Home = () => {
         </header>
         <div className='h-l-text'>
           <div className='h-g-pic'>
-            <figure className='h-g-p-1'><img src="/img-Home/group1.jpg" alt="" /></figure>
-            <figure className='h-g-p-2'><img src="/img-Home/group2.jpeg" alt="" /></figure>
-            <figure className='h-g-p-3'><img src="/img-Home/group3.jpeg" alt="" /></figure>
+            <figure className='h-g-p-1'><img src="./img-Home/group1.jpg" alt="" /></figure>
+            <figure className='h-g-p-2'><img src="./img-Home/group2.jpeg" alt="" /></figure>
+            <figure className='h-g-p-3'><img src="./img-Home/group3.jpeg" alt="" /></figure>
           </div>
           <ul>
-            <li><span>找吃飯夥伴<img src="/img-Home/home-s-right.svg" alt="" /></span></li>
-            <li><span>找工作夥伴<img src="/img-Home/home-s-right.svg" alt="" /></span></li>
-            <li><span>找踩點夥伴<img src="/img-Home/home-s-right.svg" alt="" /></span></li>
-            <li><span>找合租室友<img src="/img-Home/home-s-right.svg" alt="" /></span></li>
-            <li><span>找 Chill 伴<img src="/img-Home/home-s-right.svg" alt="" /></span></li>
+            <li><span>找吃飯夥伴<img src="./img-Home/home-s-right.svg" alt="" /></span></li>
+            <li><span>找工作夥伴<img src="./img-Home/home-s-right.svg" alt="" /></span></li>
+            <li><span>找踩點夥伴<img src="./img-Home/home-s-right.svg" alt="" /></span></li>
+            <li><span>找合租室友<img src="./img-Home/home-s-right.svg" alt="" /></span></li>
+            <li><span>找 Chill 伴<img src="./img-Home/home-s-right.svg" alt="" /></span></li>
             <button className='home-all-spot'>
               <Link to="/group" >所有活動</Link>
             </button>
@@ -147,7 +147,7 @@ const Home = () => {
           <p className='h-g-botton'>Get together Get together</p>
         </div>
         <p className='h-g-side-word-1'>nice to me you</p>
-        <figure className='h-group-line'><img src="/img-Home/group-line.svg" alt="" /></figure>
+        <figure className='h-group-line'><img src="./img-Home/group-line.svg" alt="" /></figure>
       </section>
 
       <section id='homediary'>
@@ -173,13 +173,13 @@ const Home = () => {
 
               <div className='h-d-sign'>
                 <div className='h-d-locaion'>
-                  <figure><img src="/img-Home/location.svg" alt="" /></figure>
+                  <figure><img src="./img-Home/location.svg" alt="" /></figure>
                   <p>越南/番西邦峰</p>
                 </div>
                 <p className='h-d-date'>Apr,03 2025</p>
-                <figure><img src="/img-Home/heart.svg" alt="" /></figure>
-                <figure><img src="/img-Home/chat.svg" alt="" /></figure>
-                <figure><img src="/img-Home/save.svg" alt="" /></figure>
+                <figure><img src="./img-Home/heart.svg" alt="" /></figure>
+                <figure><img src="./img-Home/chat.svg" alt="" /></figure>
+                <figure><img src="./img-Home/save.svg" alt="" /></figure>
               </div>
             </div>
           </article>
@@ -189,12 +189,12 @@ const Home = () => {
         </div>
 
         <figure className='h-d-pics'>
-          <img src="/img-Home/diary-1.jpg" alt="" />
-          <img src="/img-Home/diary-2.jpg" alt="" />
-          <img src="/img-Home/diary-3.jpg" alt="" />
+          <img src="./img-Home/diary-1.jpg" alt="" />
+          <img src="./img-Home/diary-2.jpg" alt="" />
+          <img src="./img-Home/diary-3.jpg" alt="" />
         </figure>
         <p className='h-d-side-word'>mood ...</p>
-        <figure className='h-d-boat'><img src="/img-Home/diary-boat.svg" alt="" /></figure>
+        <figure className='h-d-boat'><img src="./img-Home/diary-boat.svg" alt="" /></figure>
       </section>
     </main>
   )
