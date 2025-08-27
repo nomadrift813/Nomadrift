897a1b8e5e14e4e1a25a3a25046a7bbfe139f831
rosa161 <rosarosa161@gmail.com>
2025-08-27 15:18:07 +0800

    0827-E-updateGroupPhoto

--- src/pages/Group.jsx
+++ src/pages/Group.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Group = () => {
   return (
-    <><main>
+    <main>
       {/* Banner 區 */}
       <section id="groupBanner">
         <div className='groupSlogan'>
@@ -16,33 +16,41 @@ const Group = () => {
           </figure>
         </div>
       </section>
-    </main>
+
 
 
       {/* 內容區 */}
       {/* 主標題 */}
       <section id="group-content">
-        <div className='content-title'>
+        <div className="content-title">
           <h3>Travel solo, connect together!</h3>
           <div className="line"></div>
           <h2>漂友集合站！</h2>
         </div>
 
         {/* 各揪團按鈕 */}
-        <div id="button-list">
-        <ul className="group-buttons">
-          <li><button>全部活動</button></li>
-          <li><button>找吃飯夥伴</button></li>
-          <li><button>找工作夥伴</button></li>
-          <li><button>找踩點夥伴</button></li>
-          <li><button>找合租室友</button></li>
-          <li><button>找Chill伴</button></li>
-        </ul>
+        <div className="button-list">
+          <ul className="group-buttons">
+            <li><a href="#">全部活動</a></li>
+            <li><a href="#">找吃飯夥伴</a></li>
+            <li><a href="#">找工作夥伴</a></li>
+            <li><a href="#">找踩點夥伴</a></li>
+            <li><a href="#">找合租室友</a></li>
+            <li><a href="#">找Chill伴</a></li>
+          </ul>
         </div>
 
-        <div className='group-card'></div>
-      </section>
-    </>
+
+        <div className='group-event-card'>
+          <figure class="card-media">
+            <img src="raohe.jpg" alt="饒河夜市揪團活動縮圖" itemprop="image">
+              <figcaption class="badge">7人已報名</figcaption>
+          </figure>
+
+
+        </div>
+      </section >
+    </main>
 
   )
 }
