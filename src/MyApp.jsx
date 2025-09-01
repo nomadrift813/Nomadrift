f851e33cb90a409636f7bce9fffd946882849ea6
chiaohua <inin0112@gmail.com>
2025-09-01 11:38:25 +0800

    0901-F

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -4,7 +4,8 @@ import Home from "./pages/Home"
 import Location from "./pages/Location"
 import Group from "./pages/Group"
 import Diary from "./pages/Diary"
-import Member from "./pages/Member-data"    
+import Diary2 from "./pages/Diary2"
+import Member from "./pages/Member-data"
 import Location2 from "./pages/Location-2"
 import Location3 from "./pages/Location-3"
 import Log from "./component/Log"
@@ -15,21 +16,22 @@ import { Route, Routes } from "react-router-dom"
 const MyApp = () => {
   return (
     <div className="wrap">
-        <Nav/>
-            <Routes>
-                <Route path='/' element={<Home/>}></Route>
-                <Route path='/location' element={<Location/>}></Route>
-                <Route path='/group' element={<Group/>}></Route>
-                <Route path='/diary' element={<Diary/>}></Route>
-                <Route path='/location2' element={<Location2/>}></Route>
-                <Route path='/location3' element={<Location3/>}></Route>
-                <Route path='/group2' element={<Group2/>}></Route>
-                <Route path='/member' element={<Member/>}></Route>
-                <Route path='/log' element={<Log/>}></Route>
-                <Route path='/sign' element={<Sign/>}></Route>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/location' element={<Location />}></Route>
+        <Route path='/group' element={<Group />}></Route>
+        <Route path='/diary' element={<Diary />}></Route>
+        <Route path='/diary2' element={<Diary2 />}></Route>
+        <Route path='/location2' element={<Location2 />}></Route>
+        <Route path='/location3' element={<Location3 />}></Route>
+        <Route path='/group2' element={<Group2 />}></Route>
+        <Route path='/member' element={<Member />}></Route>
+        <Route path='/log' element={<Log />}></Route>
+        <Route path='/sign' element={<Sign />}></Route>
 
-            </Routes>
-        <Footer/>
+      </Routes>
+      <Footer />
     </div>
   )
 }
